a797f4e365db84d2cda0cd0a897371f220ff2d26
DorianCor <dorc4u@gmail.com>
2026-02-13 17:49:28 +0100

    Portfolio - Ajout favicon & title

--- src/router/index.js
+++ src/router/index.js
@@ -7,27 +7,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      meta: { title: 'Accueil' },
       component: HomeView,
     },
     {
       path: '/about',
       name: 'about',
+      meta: { title: 'A propos' },
       component: () => import('../views/AboutView.vue'),
     },
     {
       path: '/projets',
       name: 'projects',
+      meta: { title: 'Projets' },
       component: () => import('../views/ProjectsView.vue'),
     },
     {
       path: '/contact',
       name: 'contact',
+      meta: { title: 'Contact' },
       component: () => import('../views/ContactView.vue'),
     },
   ],
     scrollBehavior() {
     return { top: 0 }
   }
+})
+
+router.afterEach((to) => {
+  const pageTitle = to.meta?.title ? `${to.meta.title} | Creadox` : 'Creadox'
+  document.title = pageTitle
 })
 
 export default router
